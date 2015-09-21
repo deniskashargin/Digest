33f6b4637fd5fdbb50d4edab75bbe214aaee6012
deniskashargin <denis@kashargin.ru>
2015-09-22 00:52:16 +0300

    :)

--- assets/components/digest/connector.php
+++ assets/components/digest/connector.php
@@ -1,17 +1,22 @@
 <?php
-/** @noinspection PhpIncludeInspection */
-require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
-/** @noinspection PhpIncludeInspection */
+// For debug
+ini_set('display_errors', 1);
+ini_set('error_reporting', -1);
+// Load MODX config
+if (file_exists(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php')) {
+	require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
+}
+else {
+	require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/config.core.php';
+}
 require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
-/** @noinspection PhpIncludeInspection */
 require_once MODX_CONNECTORS_PATH . 'index.php';
-/** @var Digest $Digest */
-$Digest = $modx->getService('digest', 'Digest', $modx->getOption('digest_core_path', null, $modx->getOption('core_path') . 'components/digest/') . 'model/digest/');
-$modx->lexicon->load('digest:default');
-
-// handle request
 $corePath = $modx->getOption('digest_core_path', null, $modx->getOption('core_path') . 'components/digest/');
-$path = $modx->getOption('processorsPath', $Digest->config, $corePath . 'processors/');
+require_once $corePath . 'model/digest/digest.class.php';
+$modx->digest = new Digest($modx);
+$modx->lexicon->load('digest:default');
+/* handle request */
+$path = $modx->getOption('processorsPath', $modx->digest->config, $corePath . 'processors/');
 $modx->request->handleRequest(array(
 	'processors_path' => $path,
 	'location' => '',
--- assets/components/digest/js/mgr/sections/home.js
+++ assets/components/digest/js/mgr/sections/home.js
@@ -1,11 +1,12 @@
-Digest.page.Home = function (config) {
+Digest.page.Home = function(config) {
 	config = config || {};
-	Ext.applyIf(config, {
+	Ext.applyIf(config,{
 		components: [{
-			xtype: 'digest-panel-home', renderTo: 'digest-panel-home-div'
+			xtype: 'digest-panel-home'
+			,renderTo: 'digest-panel-home-div'
 		}]
 	});
-	Digest.page.Home.superclass.constructor.call(this, config);
+	Digest.page.Home.superclass.constructor.call(this,config);
 };
-Ext.extend(Digest.page.Home, MODx.Component);
-Ext.reg('digest-page-home', Digest.page.Home);
+Ext.extend(Digest.page.Home,MODx.Component);
+Ext.reg('digest-page-home',Digest.page.Home);
