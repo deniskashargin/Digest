03c7a5bed7262c65406f18bd5651c3df218e39c7
deniskashargin <denis@kashargin.ru>
2015-09-22 00:52:33 +0300

    :)

--- core/components/digest/controllers/home.class.php
+++ core/components/digest/controllers/home.class.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The home manager controller for Digest.
  *
@@ -7,32 +6,22 @@
 class DigestHomeManagerController extends DigestMainController {
 	/* @var Digest $Digest */
 	public $Digest;
-
-
 	/**
 	 * @param array $scriptProperties
 	 */
 	public function process(array $scriptProperties = array()) {
 	}
-
-
 	/**
 	 * @return null|string
 	 */
 	public function getPageTitle() {
 		return $this->modx->lexicon('digest');
 	}
-
-
 	/**
 	 * @return void
 	 */
 	public function loadCustomCssJs() {
-		$this->addCss($this->Digest->config['cssUrl'] . 'mgr/main.css');
-		$this->addCss($this->Digest->config['cssUrl'] . 'mgr/bootstrap.buttons.css');
-		$this->addJavascript($this->Digest->config['jsUrl'] . 'mgr/misc/utils.js');
 		$this->addJavascript($this->Digest->config['jsUrl'] . 'mgr/widgets/items.grid.js');
-		$this->addJavascript($this->Digest->config['jsUrl'] . 'mgr/widgets/items.windows.js');
 		$this->addJavascript($this->Digest->config['jsUrl'] . 'mgr/widgets/home.panel.js');
 		$this->addJavascript($this->Digest->config['jsUrl'] . 'mgr/sections/home.js');
 		$this->addHtml('<script type="text/javascript">
@@ -41,8 +30,6 @@ class DigestHomeManagerController extends DigestMainController {
 		});
 		</script>');
 	}
-
-
 	/**
 	 * @return string
 	 */
--- core/components/digest/index.class.php
+++ core/components/digest/index.class.php
@@ -1,42 +1,33 @@
 <?php
-
 /**
  * Class DigestMainController
  */
 abstract class DigestMainController extends modExtraManagerController {
 	/** @var Digest $Digest */
 	public $Digest;
-
-
 	/**
 	 * @return void
 	 */
 	public function initialize() {
 		$corePath = $this->modx->getOption('digest_core_path', null, $this->modx->getOption('core_path') . 'components/digest/');
 		require_once $corePath . 'model/digest/digest.class.php';
-
 		$this->Digest = new Digest($this->modx);
-		//$this->addCss($this->Digest->config['cssUrl'] . 'mgr/main.css');
+		$this->addCss($this->Digest->config['cssUrl'] . 'mgr/main.css');
 		$this->addJavascript($this->Digest->config['jsUrl'] . 'mgr/digest.js');
-		$this->addHtml('
-		<script type="text/javascript">
+		$this->addHtml('<script type="text/javascript">
+		Ext.onReady(function() {
 			Digest.config = ' . $this->modx->toJSON($this->Digest->config) . ';
 			Digest.config.connector_url = "' . $this->Digest->config['connectorUrl'] . '";
-		</script>
-		');
-
+		});
+		</script>');
 		parent::initialize();
 	}
-
-
 	/**
 	 * @return array
 	 */
 	public function getLanguageTopics() {
 		return array('digest:default');
 	}
-
-
 	/**
 	 * @return bool
 	 */
@@ -44,13 +35,10 @@ abstract class DigestMainController extends modExtraManagerController {
 		return true;
 	}
 }
-
-
 /**
  * Class IndexManagerController
  */
 class IndexManagerController extends DigestMainController {
-
 	/**
 	 * @return string
 	 */
